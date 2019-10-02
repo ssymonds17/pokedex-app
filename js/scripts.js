cf740936a137915fd43f4e5a66461742251be01c
Sam Symonds <samuelsymonds2017@gmail.com>
2019-10-02 16:35:06 +0100

    Update scripts.js
    If confirmed, this commit will create a loop to print the names of the existing 3 Pokemon within the variable and add text to the Pokemon on Evolution stage level 3.

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,10 @@ var repository = [
     stage: 3
   }
 ];
+
+for (var i = 0; i < repository.length; i++) {
+  document.write("<br>" + repository[i].name + "<br>");
+  if (i < repository.length && repository[i].stage === 3) {
+    document.write('Final Evolutionary Stage');
+  }
+}
