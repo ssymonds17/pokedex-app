dc91db60654233ccbe14977a4ff435890677281d
Sam Symonds <samuelsymonds2017@gmail.com>
2019-10-02 14:55:05 +0100

    Update scripts.js
    If confirmed, this commit will remove all previous code and replace with new variable and initial 3 objects (first 3 pokemon).

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-alert('Hello World');
-
-var favouriteFood = 'Pizza';
-
-document.write(favouriteFood);
+var repository = [
+  {
+    name: "Bulbasaur",
+    type: ['grass', 'poison'],
+    stage: 1
+  },
+  {
+    name: "Ivysaur",
+    type: ['grass', 'poison'],
+    stage: 2
+  },
+  {
+    name: "Venusaur",
+    type: ['grass', 'poison'],
+    stage: 3
+  }
+];
