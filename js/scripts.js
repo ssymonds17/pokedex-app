1ef545918c254eb32e4d34344e7c28af658b666b
Sam Symonds <samuelsymonds2017@gmail.com>
2019-10-03 16:31:49 +0100

    Added forEach loop
    If confirmed, this commit will replace the for loop with a forEach loop.
    Also minor change to formatting of second Pokemon type ('poison' to ' Poison'

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,32 @@
 var repository = [
   {
     name: "Bulbasaur",
-    type: ['grass', 'poison'],
+    type: ['Grass', ' Poison'],
     stage: 1
   },
   {
     name: "Ivysaur",
-    type: ['grass', 'poison'],
+    type: ['Grass', ' Poison'],
     stage: 2
   },
   {
     name: "Venusaur",
-    type: ['grass', 'poison'],
+    type: ['Grass', ' Poison'],
     stage: 3
   }
 ];
 
-for (var i = 0; i < repository.length; i++) {
-  document.write("<br>" + repository[i].name + "<br>");
-  if (i < repository.length && repository[i].stage === 3) {
-    document.write('Final Evolutionary Stage');
-  }
+// for (var i = 0; i < repository.length; i++) {
+//   document.write("<br>" + repository[i].name + "<br>");
+//   if (i < repository.length && repository[i].stage === 3) {
+//     document.write('Final Evolutionary Stage');
+//   }
+// }
+
+repository.forEach(function(repository) {
+  document.write('Name: ' + repository.name + ' Type: '
+  + repository.type + ' Evolutionary Stage: ' + repository.stage + '<br>');
+ if (repository.stage === 3) {
+  document.write('Final Evolutionary Stage');
 }
+});
