765e6403152890aba79e0c0813f6010b7f0a31a8
Sam Symonds <samuelsymonds2017@gmail.com>
2019-10-09 15:19:00 +0100

    Wrap repository in IIFE
    If confirmed, this commit will wrap the repository variable within an IIFE to control what is globally accessible.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-var repository = [
+// Wraps repository within IIFE
+var pokemonRepository = (function () {
+ var repository = [
   {
     name: "Bulbasaur",
     type: ['Grass', ' Poison'],
@@ -14,12 +16,31 @@ var repository = [
     type: ['Grass', ' Poison'],
     stage: 3
   }
-];
+ ];
+// Adds new Pokemon to var repository
+ function add(pokemon) {
+// Checks if the input is an object
+   if (typeof pokemon === 'object')repository.push(pokemon);
+ }
 
-repository.forEach(function(pokeList) {
-  document.write('Name: ' + pokeList.name + ' Type: '
+ function catchAll() {
+   return repository;
+ }
+
+ return {
+   add: add,
+   catchAll: catchAll
+ };
+})();
+
+pokemonRepository.catchAll().forEach(function(pokeList) {
+ document.write('Name: ' + pokeList.name + ' Type: '
   + pokeList.type + ' Evolutionary Stage: ' + pokeList.stage + '<br>');
  if (pokeList.stage === 3) {
   document.write('Final Evolutionary Stage');
 }
 });
+
+// Create a search function that allows access to Pokemon by name
+// (Unfinished)
+// var pokemonSearch = pokemonRepository.filter(function(pokeName);
